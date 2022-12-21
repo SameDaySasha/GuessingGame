165569ee16d9f62032c3caa1b90e9006b74c7c3a
SameDaySasha <111303031+SameDaySasha@users.noreply.github.com>
2022-12-20 20:22:56 -0500

    final

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,28 @@
+const readLine = require(`node:readline`)
+const { stdin: input, stdout: output } = require('node:process')
+const rl = readLine.createInterface({ input, output})
+
+rl.question("Enter a guess: ",  function askGuess(answer) {
+    let num = Number(answer)
+    if(checkGuess(num) === true){
+        rl.close()
+    } else {
+        rl.question(' Try again. ', askGuess)
+    }
+        
+        
+        
+    
+
+
+
+}) 
+
+
+
 const secretNumber = 42
 
+ 
 
 const checkGuess = number => {
     if(number > secretNumber){
@@ -10,11 +33,24 @@ const checkGuess = number => {
         console.log(`Too low.`)
         return false
     } 
-    if   (number === secretNumber){
+    if   (number === secretNumber){         
         console.log(`Correct!`)
         return true
         
-    }
-      
+    } 
     
+ 
+
 }
+
+
+
+// console.log(checkGuess(91))
+// console.log(checkGuess(19))
+// console.log(checkGuess(13))
+// console.log(checkGuess(6))
+// console.log(checkGuess(42))
+// console.log(checkGuess(978))
+// console.log(checkGuess(55))
+
+
